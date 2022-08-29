94ea8890b21b60c20f8aa422847d35e279fb0408
William <49433145+iyolo@users.noreply.github.com>
2022-08-29 14:48:56 +0800

    Fix div margin

--- js/award.js
+++ js/award.js
@@ -5,18 +5,18 @@ $(document).ready(function(){
     var name = ["ZhiQing Guo","Henry Yu","Darek Deng","Denis Xu","Cissy Xiang"]
 
     $.each(pictures, function(value) {
-        $("div.imageDiv").append(' <div class="col-lg-3 col-md-4">\n' +
+        $("div.imageDiv").append(' <div class="col-lg-3 col-md-4" style="margin-bottom: 30px">\n' +
             '                <div\n' +
             '                  class="single_performer wow fadeInUp"\n' +
             '                  data-wow-duration="1s"\n' +
-            '                  data-wow-delay=".4s"\n' +
+            '                  data-wow-delay=".4s" style="margin-bottom: 30px"\n' +
             '                >\n' +
             '                  <div data-tilt class="thumb">\n' +
             '                    <img src="img/2021award/'+ pictures[value] +'.jpg" alt="" />\n' +
             '                  </div>\n' +
             '                  <div class="performer_heading">\n' +
             '                    <h4>'+ name[value] +'</h4>\n' +
-            '                    <span class="title">'+ department[value] +'</span><br />\n' +
+            '                    <span class="title">'+ department[value] +'</span>\n' +
             '                  </div>\n' +
             '                </div>\n' +
             '              </div>');
