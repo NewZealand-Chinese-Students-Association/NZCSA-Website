10485e5ff17f7854ee03dbbec40ae482c6b2c19b
KirstyGong <66720955+KirstyGong@users.noreply.github.com>
2022-08-31 21:24:06 +1200

    Awardpage updated (#25)
    * Add files via upload
    
    Adding Award Page and making changes to other pages' hearder
    
    * Add files via upload
    
    adding new images for award page
    
    * Delete 2021award directory
    
    * Fix navigation
    
    * Change jquery structure
    
    * Fix div margin
    
    * Add Renee
    
    * minor error
    
    Co-authored-by: Tony Shu <110316849+tshu1234@users.noreply.github.com>
    Co-authored-by: William <49433145+iyolo@users.noreply.github.com>

--- js/award.js
+++ js/award.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
 
-    var pictures = [ "zhiqing", "henry","darek","denis","cissy"];
-    var department = ["Department of Information Technology","Department of Operations","Department of Operations","Department of Human Resources","Department of Marketing"]
-    var name = ["ZhiQing Guo","Henry Yu","Darek Deng","Denis Xu","Cissy Xiang"]
+    var pictures = [ "zhiqing", "henry","darek","denis","cissy","renee"];
+    var department = ["Department of Information Technology","Department of Marketing","Department of Operations","Department of Business Development","Department of Marketing","Department of Human Resources"]
+    var name = ["ZhiQing Guo","Henry Yu","Darek Deng","Denis Xu","Cissy Xiang","Renee Fang"]
 
     $.each(pictures, function(value) {
-        $("div.imageDiv").append(' <div class="col-lg-3 col-md-4">\n' +
+        $("div.imageDiv").append(' <div class="col-lg-3 col-md-4" style="margin-bottom: 1px">\n' +
             '                <div\n' +
             '                  class="single_performer wow fadeInUp"\n' +
             '                  data-wow-duration="1s"\n' +
-            '                  data-wow-delay=".4s"\n' +
+            '                  data-wow-delay=".4s" style="margin-bottom: 30px"\n' +
             '                >\n' +
             '                  <div data-tilt class="thumb">\n' +
             '                    <img src="img/2021award/'+ pictures[value] +'.jpg" alt="" />\n' +
             '                  </div>\n' +
-            '                  <div class="performer_heading">\n' +
+            '                  <div class="performer_heading" style="margin-top: 10px">\n' +
             '                    <h4>'+ name[value] +'</h4>\n' +
-            '                    <span class="title">'+ department[value] +'</span><br />\n' +
+            '                    <span class="title">'+ department[value] +'</span>\n' +
             '                  </div>\n' +
             '                </div>\n' +
             '              </div>');
